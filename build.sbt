f96de87d9bc6a83a21abc7c1fa90770bf2f75396
Stefano Babini <stefano.babini5@studio.unibo.it>
2025-06-04 15:53:59 +0200

    working akka

--- build.sbt
+++ build.sbt
@@ -3,21 +3,26 @@ val scala3Version = "3.7.0"
 resolvers += "Akka library repository".at("https://repo.akka.io/maven")
 lazy val AkkaVersion = "2.10.6"
 fork := true
-
+lazy val akkaGroup = "com.typesafe.akka"
 lazy val root = project
   .in(file("."))
   .settings(
-    name := "git status",
+    name := "assignment-03",
     version := "0.1.0-SNAPSHOT",
 
     scalaVersion := scala3Version,
 
     libraryDependencies ++= Seq(
-      "org.scalameta" %% "munit" % "1.0.0" % Test,
-      "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
-      "com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test,
-      "org.scalatest" %% "scalatest" % "3.2.15" % Test)
+      "org.scalameta" %% "munit" % "1.1.1" % Test,
+      akkaGroup %% "akka-actor-typed" % AkkaVersion,
+      akkaGroup %% "akka-actor-testkit-typed" % AkkaVersion % Test,
+      "org.scalatest" %% "scalatest" % "3.2.19" % Test,
+      "org.scalatest" %% "scalatest" % "3.2.19" % Test,
+      "org.scala-lang.modules" %% "scala-swing" % "3.0.0",
+      "ch.qos.logback" % "logback-classic" % "1.5.18"
+    ),
   )
+
 
 
 // Run in a separate JVM, to make sure sbt waits until all threads have
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,21 @@
-import akka.actor.typed.ActorRef
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.Behavior
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
 
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
-
-def msg = "I was compiled by Scala 3. :)"
-
-object HelloWorld {
+// "Actor" module definition
+object HelloActor:
+  // "API", i.e. message that actors should received / send
   final case class Greet(whom: String, replyTo: ActorRef[Greeted])
+
   final case class Greeted(whom: String, from: ActorRef[Greet])
 
-  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
+  // Behaviour factory, i.e how the actor react to messages
+  def apply(): Behavior[Greet] = Behaviors.receive: (context, message) =>
     context.log.info("Hello {}!", message.whom)
     message.replyTo ! Greeted(message.whom, context.self)
     Behaviors.same
-  }
-}
+
+object HelloWorldAkkaTyped extends App:
+  private val system: ActorSystem[HelloActor.Greet] = ActorSystem(HelloActor(), name = "hello-world")
+  system ! HelloActor.Greet("Akka Typed", system.ignoreRef)
+  Thread.sleep(5000)
+  system.terminate()

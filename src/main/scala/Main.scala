369b6d8c42e45a6fcb15c5c874e2b33470ee0898
Stefano Babini <stefano.babini5@studio.unibo.it>
2025-06-09 17:31:22 +0200

    sitll to optimize something

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,12 +4,12 @@ import akka.actor.typed.scaladsl.Behaviors
 def rootBehavior: Behavior[Unit] =
   Behaviors.setup { context =>
     val view = pcd.ass01.View.ScalaBoidsView(800, 800)
-    val boidsList = for (i <- 1 to 400) yield context.spawnAnonymous(BoidActor(view))
+    // Props for the custom dispatcher
+    val boidDispatcher = DispatcherSelector.fromConfig("boid-dispatcher")
+    val boidsList = for (i <- 1 to 500) yield context.spawnAnonymous(BoidActor(view), DispatcherSelector.fromConfig("boid-dispatcher"))
     implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
     //wait for the boids to be created
     //sleep for a short duration to ensure all actors are initialized
-    boidsList.foreach(_ ! BoidActor.VelocityTick)
-    boidsList.foreach(_ ! BoidActor.PositionTick)
     context.system.scheduler.scheduleAtFixedRate(
       initialDelay = scala.concurrent.duration.Duration(5, "seconds"),
       interval = scala.concurrent.duration.FiniteDuration(40, "millis") // Adjust the interval as needed
